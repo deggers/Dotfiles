adcf73d56823cd713c947f617a07289c13455145
dustyn <dustyneggers@gmail.com>
2020-05-27 21:58:15 +0200

    some fixes

--- emacs/init.el
+++ emacs/init.el
@@ -1,2 +1,15 @@
 (setq vc-follow-symlinks t)
 (org-babel-load-file "~/.emacs.d/configuration.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(haskell-mode which-key use-package undo-fu rainbow-delimiters org-plus-contrib helm general evil elisp-format dracula-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
