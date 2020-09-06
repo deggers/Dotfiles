a3eab9ab74017d7a9e0ae659c80a213eb9d9b0f1
dustyn <dustyneggers@gmail.com>
2020-09-06 12:06:38 +0200

    Doom-Theme and Doom-Modeline

--- emacs/init.el
+++ emacs/init.el
@@ -6,7 +6,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(haskell-mode which-key use-package undo-fu rainbow-delimiters org-plus-contrib helm general evil elisp-format dracula-theme)))
+   '(doom-modeline doom-themes diminish winum haskell-process haskell-interactive-mode haskell-mode which-key use-package undo-fu rainbow-delimiters org-plus-contrib helm general evil elisp-format dracula-theme))
+ '(winum-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
