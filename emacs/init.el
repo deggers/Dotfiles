ace8c35c0478904abaf13b4f6ebab862fea506fd
Dustyn Eggers <8664081+deggers@users.noreply.github.com>
2020-12-14 19:14:10 +0100

    Adds nix-mode and colored-layout

--- emacs/init.el
+++ emacs/init.el
@@ -5,8 +5,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(epg-gpg-program "/usr/bin/gpg2")
  '(package-selected-packages
-   '(neotree treemacs-evil treemacs-magit treemacs-icons-dired treemacs-projectile treemacs projectile yasnippet org-tempo yas-snippet evil-multiedit spaceline-config spaceline-all-the-icons modus-operandi-theme modus-vivendi-theme avy git-gutter imenu-list doom-modeline doom-themes diminish winum haskell-process haskell-interactive-mode haskell-mode which-key use-package undo-fu rainbow-delimiters org-plus-contrib helm general evil elisp-format dracula-theme))
+   '(color-identifiers-mode neotree treemacs-evil treemacs-magit treemacs-icons-dired treemacs-projectile treemacs projectile yasnippet org-tempo yas-snippet evil-multiedit spaceline-config spaceline-all-the-icons modus-operandi-theme modus-vivendi-theme avy git-gutter imenu-list doom-modeline doom-themes diminish winum haskell-process haskell-interactive-mode haskell-mode which-key use-package undo-fu rainbow-delimiters org-plus-contrib helm general evil elisp-format dracula-theme))
  '(winum-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
